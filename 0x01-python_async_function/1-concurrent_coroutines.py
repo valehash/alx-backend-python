dd052f8983d9bfa34769ba2096dd662a73996b37
vale-hash <newlechim@gmail.com>
2024-08-06 12:50:14 +0100

    task 1 running concurrently newline added

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import asyncio
-wait_rand=__import__('0-basic_async_syntax').wait_random
 """
     an async routine called wait_n 
     that takes in 2 int arguments (in this order): n and max_delay. 
@@ -12,6 +11,7 @@ wait_rand=__import__('0-basic_async_syntax').wait_random
     returns:  return the list of all the delays (float values) in ascending order.
 """
 
+wait_rand=__import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """A function that list all the delays created(sorted) in seconds"""

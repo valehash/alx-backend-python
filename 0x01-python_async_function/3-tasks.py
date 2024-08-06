0055acd76eaa4fbda40052b694281ee09adcc012
vale-hash <newlechim@gmail.com>
2024-08-06 08:23:02 +0100

    type done

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,8 +2,7 @@
 import asyncio
 tasks= __import__("0-basic_async_syntax").wait_random
 
-def task_wait_random(max_delay : int):
+def task_wait_random(max_delay : int) -> object:
     c = asyncio.create_task(tasks(max_delay))
-
     return c
 

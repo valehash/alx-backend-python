a917f2c949c5b318d14b9400f0ce78815dd480fd
vale-hash <newlechim@gmail.com>
2024-08-06 16:41:21 +0100

    task 0 async_generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Write a coroutine called async_generator that takes no arguments.
+
+The coroutine will loop 10 times, each time asynchronously wait 1 second, 
+then yield a random number between 0 and 10. Use the random module. 
+"""
+
+import asyncio
+import random
+
+async def async_generator():
+    for i in range(1, 10):
+        await asyncio.sleep(1)
+        yield(random.uniform(1 ,10))
+        i += 1
+

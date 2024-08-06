6453ef0aa54d854e0c32dc3a85145ee52a6d145b
vale-hash <newlechim@gmail.com>
2024-08-06 12:48:21 +0100

    task 1 running concurrently newline added

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,10 +14,11 @@ wait_rand=__import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    """A function that list all the delays created in seconds"""
+    """A function that list all the delays created(sorted) in seconds"""
     array = []
     routine = [wait_rand(max_delay) for i in range(n)]
     for routine in asyncio.as_completed(routine):
         val = await(routine)
         array.append(val)
     return array
+

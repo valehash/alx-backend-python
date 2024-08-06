4de30239fe3a2d5f3cdd1bfcb7c995bfb8690a4c
vale-hash <newlechim@gmail.com>
2024-08-06 12:52:22 +0100

    reformated task2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """
 From the previous file, import wait_n into 2-measure_runtime.py.
-
 Create a measure_time function with integers n and max_delay as arguments that measures the total execution time for wait_n(n, max_delay), and returns total_time / n. Your function should return a float.
-
 Use the time module to measure an approximate elapsed time.
 """
 

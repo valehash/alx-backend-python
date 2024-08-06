c545374d0062ec82632039ed246216b09803933f
vale-hash <newlechim@gmail.com>
2024-08-06 12:55:19 +0100

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@ import random as rand
 import asyncio
 
 async def wait_random(max_delay=10):
-    delay=(rand.uniform(0, max_delay))
+    delay = rand.randint(0, max_delay)
     await asyncio.sleep(delay)
     return delay
 

ddf214b13cd1327a2435ab9ebe6993aa6acf54e0
vale-hash <newlechim@gmail.com>
2024-08-06 08:54:06 +0100

    task wait random done

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,11 +5,14 @@ Write a function
 (do not create an async function, use the regular function syntax to do this) 
 task_wait_random that takes an integer max_delay and returns a asyncio.Task.
 """
+
+
 import asyncio
 tasks= __import__("0-basic_async_syntax").wait_random
 
+
 def task_wait_random(max_delay : int) -> asyncio.Task:
-    """asyncio task routine """
+    """Function that takes in a max_delay and returns a class async_task """
     c = asyncio.create_task(tasks(max_delay))
     return c
 

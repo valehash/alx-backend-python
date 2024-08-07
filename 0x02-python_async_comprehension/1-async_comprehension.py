b6c8d505265a979d5f599bb4dad8a56ef51cb40d
vale-hash <newlechim@gmail.com>
2024-08-07 09:22:05 +0100

    task 1 using list comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 """
-Import async_generator from the previous task and then write 
+Import async_generator from the previous task and then write
 a coroutine called async_comprehension that takes no arguments.
-The coroutine will collect 10 random numbers using 
-an async comprehensing over async_generator, 
+The coroutine will collect 10 random numbers using
+an async comprehensing over async_generator,
 then return the 10 random numbers.
 """
 
 
 generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension():
-    array = []
-    async for i in generator():
-        array.append(i)
-    return array
+
+async def async_comprehension() -> list[float]:
+    """
+    Function that adds the async calls generat4ed into an array
+    """
+    new_list = [i async for i in generator()]
+    return new_list
